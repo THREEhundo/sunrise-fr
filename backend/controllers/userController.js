c36157b2c57809f62ccdc5a6eaace9e8fd17a68d
threehund0 <samuelbaik87@gmail.com>
2024-04-16 12:37:18 -0700

    added additional routes to api/users

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,4 +7,32 @@ const authUser = expressAsyncHandler(async (req, res) => {
 	res.status(200).json({ message: 'Auth User' })
 })
 
-export { authUser }
+// @desc	Register a new user
+// route 	POST /api/users
+// @access	Public
+const registerUser = expressAsyncHandler(async (req, res) => {
+	res.status(200).json({ message: 'Register User' })
+})
+
+// @desc	Logout user
+// route 	POST /api/users/logout
+// @access	Public
+const logoutUser = expressAsyncHandler(async (req, res) => {
+	res.status(200).json({ message: 'Logout User' })
+})
+
+// @desc	Get user profile
+// route 	GET /api/users/profile
+// @access	Private
+const getUserProfile = expressAsyncHandler(async (req, res) => {
+	res.status(200).json({ message: ' User Profile' })
+})
+
+// @desc	Update user profile
+// route 	PUT /api/users/profile
+// @access	Private
+const updateUserProfile = expressAsyncHandler(async (req, res) => {
+	res.status(200).json({ message: ' User Profile Updated' })
+})
+
+export { authUser, registerUser, logoutUser, getUserProfile, updateUserProfile }
