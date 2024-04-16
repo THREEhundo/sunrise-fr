8e693b5eb655d3ce34afb899f708b211e51a653b
threehund0 <samuelbaik87@gmail.com>
2024-04-15 19:05:07 -0700

    removed error test

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,8 +4,6 @@ import expressAsyncHandler from 'express-async-handler'
 // route 	POST /api/users/auth
 // @access	Public
 const authUser = expressAsyncHandler(async (req, res) => {
-	res.status(401)
-	throw new Error('Something went wrong')
 	res.status(200).json({ message: 'Auth User' })
 })
 
