c514b30b478230576623a5fac40a1bb7d8bb2cb2
threehund0 <samuelbaik87@gmail.com>
2024-04-15 19:03:21 -0700

    fixed bug and testing error middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,6 +4,8 @@ import expressAsyncHandler from 'express-async-handler'
 // route 	POST /api/users/auth
 // @access	Public
 const authUser = expressAsyncHandler(async (req, res) => {
+	res.status(401)
+	throw new Error('Something went wrong')
 	res.status(200).json({ message: 'Auth User' })
 })
 
