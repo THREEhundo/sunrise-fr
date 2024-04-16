19ee31b32ae36eb7a99ac0948ddc4257454ae51e
threehund0 <samuelbaik87@gmail.com>
2024-04-16 12:44:23 -0700

    added routes & handlers to userRoutes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -9,5 +9,9 @@ import {
 } from '../controllers/userController.js'
 
 router.post('/auth', authUser)
+router.post('/', registerUser)
+router.post('/logout', logoutUser)
+router.get('/profile', getUserProfile)
+router.put('/profile', updateUserProfile)
 
 export default router
