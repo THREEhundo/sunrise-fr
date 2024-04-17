1b6c90cc59e528188ae22100ee64c74b7ce11651
threehund0 <samuelbaik87@gmail.com>
2024-04-17 12:40:03 -0700

    added middleware function to encrypt user password

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bcryptjs'
 
 const userSchema = mongoose.Schema(
 	{
@@ -20,5 +21,16 @@ const userSchema = mongoose.Schema(
 		timestamps: true
 	}
 )
+
+// Middleware for before saving using bcrypt
+userSchema.pre('save', async function (next) {
+	if ((!this, isModified('password'))) {
+		next()
+	}
+
+	// hash password
+	const salt = await bcrypt.genSalt(10)
+	this.password = await bcrypt.hash(this.password, salt)
+})
 
 const User = mongoose.model('User', userSchema)
