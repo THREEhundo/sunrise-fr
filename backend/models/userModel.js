6405b7819013a6f38ab845f875cc670ac105e233
threehund0 <samuelbaik87@gmail.com>
2024-04-22 17:02:23 -0700

    created password check method on User model

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -33,4 +33,8 @@ userSchema.pre('save', async function (next) {
 	this.password = await bcrypt.hash(this.password, salt)
 })
 
+userSchema.methods.matchPasswords = async function (enteredPassword) {
+	return await bcrypt.compare(enteredPassword, this.password)
+}
+
 const User = mongoose.model('User', userSchema)
