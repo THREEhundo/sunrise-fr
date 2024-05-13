c82bdd08b7fffeaebdc5c0336a2f55f3b5f37b40
threehund0 <samuelbaik87@gmail.com>
2024-05-12 18:55:10 -0700

    fixed user error in controller for registerUser function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -36,7 +36,7 @@ const registerUser = expressAsyncHandler(async (req, res) => {
 		throw new Error('User already exists')
 	}
 
-	const user = new User.create({
+	const user = await User.create({
 		name,
 		email,
 		password
